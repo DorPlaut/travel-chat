ecf4bfea7e5d276215693aae4d5196f0565ebfe7
dorplaut <dorplaut@gmail.com>
2025-04-14 03:50:12 +0300

    Update passportConfig.js

--- api/config/passportConfig.js
+++ api/config/passportConfig.js
@@ -34,7 +34,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/api/auth/google/callback',
+      // callbackURL: '/api/auth/google/callback',
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
     (accessToken, refreshToken, profile, done) => {
       // Pass the full profile to have access to all user information
